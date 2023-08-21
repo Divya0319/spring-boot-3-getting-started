304c31ad585744d919104f63764f1443984de776
Divya Gupta <divygupta0319@gmail.com>
2023-08-21 18:24:41 +0530

    Initial commit, with a simple blank, but working Spring boot 3 application

---
+++ src/test/java/com/example/springboot3gettingstarted/SpringBoot3GettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3gettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3GettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
